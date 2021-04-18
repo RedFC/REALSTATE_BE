1a0f0956aa71d988a73cff72d6236d4811bf445d
Saad Jawaid <33807729+RedFC@users.noreply.github.com>
2021-04-18 19:48:17 +0500

    update

--- Controller/extras/getImages.js
+++ Controller/extras/getImages.js
@@ -1,7 +1,5 @@
 const db = require("../../Model");
 const imagedata = db.imageData;
-const SliderData = db.Sliderdata;
-
 exports.getAllimagesByTypeAndTypeId = async (type, typeid) => {
     let ids = [];
     let getImages = await imagedata.findAll({
@@ -12,18 +10,9 @@ exports.getAllimagesByTypeAndTypeId = async (type, typeid) => {
         }
     });
     getImages.map(x => ids.push(x.imageId));
-    let getIndexs = await SliderData.findAll({
-        raw: true,
-        where: {
-            imageId: ids
-        },
-        order : [
-            ['sliderIndex','ASC']
-        ]
-    });
+
     return {
-        getImages,
-        getIndexs
+        getImages
     }
 
 }
