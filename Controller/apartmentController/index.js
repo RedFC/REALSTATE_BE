5239efc197fe8677211fa06890d14a3a1ee35adf
Saad Jawaid <33807729+RedFC@users.noreply.github.com>
2021-04-18 21:00:43 +0500

    Update index.js

--- Controller/apartmentController/index.js
+++ Controller/apartmentController/index.js
@@ -4,12 +4,15 @@ const { validate } = require("../../Model/apartments.model");
 
 const apartment = db.ApartmentModel; 
 const Op = db.Sequelize.Op;
+const FindPermission = require("../extras/FindPermission");
 
 class ApartMentController {
 
   create = async (req, res) => {
     
     try {
+        let getPermission = await FindPermission(req.user.id);
+        if(getPermission.canCreateProperty){
         let {error} = validate(req.body);
         if (error) return res.send(error.details[0].message);
 
@@ -24,6 +27,10 @@ class ApartMentController {
         let createApartment = await apartment.create(schema);
         if(createApartment){
             res.send({message : "succesfully Createad",data : createApartment})
+        }
+    }
+        else{
+            res.status(403).send({message : "You Don't Have Permission"})
         }
 
     } catch (error) {
